ab27d49e426c837cfff757c94886a8f29fcd4bc3
AcademiccMacharia <machariab19@gmail.com>
2023-06-12 15:48:44 +0300

    implementing an update feature

--- server.js
+++ server.js
@@ -8,18 +8,7 @@ app.use(express.json());
 
 app.use('/', router);
 
-//updating a todo
-// app.patch('/todoTasks/:id', (req, res)=>{
-//     const id = req.params.id;
-//     const index = todoTasks.findIndex(todo=>todo.id === Number(id));
-//     todoTasks[index] = {...todoTasks[index], ...req.body};
-//     res.status(200).json({
-//         statusCode: 'success',
-//         message: 'todo updated successfully',
-//         results: todoTasks[index]
-//     })
-// }
-//getting a single todo
+
 
 app.use('*', (req, res)=>{
     res.status(404).json({
